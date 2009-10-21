3177ab9dd95696682cefdc4384951702cd388fc8
Spencer E. Olson <olsonse@olsonse-laptop.(none)>
2009-10-21 17:02:33 -0600

    forgot to update docs in property::charge

--- src/chimp/property/charge.h
+++ src/chimp/property/charge.h
@@ -7,7 +7,10 @@
 namespace chimp {
   namespace property {
 
+    /** Namespace for the properties tag classes that provide the xpath/xml
+     * labels/queries. */
     namespace prop_tag {
+      /** The tag class for charge. */
       template < unsigned int i = 0>
       struct charge { static const char * label; };
 
@@ -22,8 +25,10 @@ namespace chimp {
      * to reference the values by P0::value, P1::value, etc. */
     typedef Generic<int, prop_tag::charge<> > charge_t;
     struct charge : charge_t {
+      /** Typedef of derivative of Generic class--required by property::Add. */
       typedef charge_t super;
 
+      /** Mass property constructor.  By default, this sets charge to 'zero'. */
       charge(const charge_t::value_type & c = charge_t().value) : charge_t(c) {}
     };
 
