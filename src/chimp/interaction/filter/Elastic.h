d0cd3f661a249d5b65e127c4004bd05cf8b2a39c
Spencer E. Olson <olsonse@olsonse-laptop.(none)>
2009-12-15 23:21:59 -0700

    added warranty/license header to all source files

--- src/chimp/interaction/filter/Elastic.h
+++ src/chimp/interaction/filter/Elastic.h
@@ -1,3 +1,26 @@
+/*==============================================================================
+ * Public Domain Contributions 2009 United States Government                   *
+ * as represented by the U.S. Air Force Research Laboratory.                   *
+ * Copyright (C) 2006, 2008 Spencer E. Olson                                   *
+ *                                                                             *
+ * This file is part of CHIMP                                                  *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify it     *
+ * under the terms of the GNU Lesser General Public License as published by    *
+ * the Free Software Foundation, either version 3 of the License, or (at your  *
+ * option) any later version.                                                  *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public        *
+ * License for more details.                                                   *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                             *
+ -----------------------------------------------------------------------------*/
+
+
 /** \file
  * Declaration of filter::Elastic class. 
  */
