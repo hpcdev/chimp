f0f8820b24aef22f44c15294653a7a570eac475a
Spencer E. Olson <olsonse@asok>
2010-02-23 07:54:56 -0700

    updated supported platforms page in api docs

--- docs/api/mainpage.h
+++ docs/api/mainpage.h
@@ -135,13 +135,45 @@ idea for documenting fidelity of the data that we will be distributing.
 
 //-----------------------------------------------------------
 /** \page chimp_platforms Supported Platforms
+  <h3>The following table represents the various platform, compiler, and
+  operating system distribution combinations for which compilation has been
+  shown to finish successfully.</h3>
+
   <table border=1>
     <tr><th>Platform</th>          <th>Compiler</th>     <th>Version</th><th>OS Distribution</th>  <th>Status</th></tr>
-    <tr><td rowspan="4">Linux</td> <td rowspan="2">GCC</td><td>4.1.2</td><td>RedHat/Centos 5.3</td><td>Supported</td></tr>
-    <tr>                                                   <td>4.4.1</td><td>Ubuntu 9.10</td>      <td>Supported</td></tr>
-    <tr>           <td rowspan="2">Pathscale</td><td rowspan="2">3.2</td><td>Cray XT4</td>         <td>Supported</td></tr>
+    <tr><td rowspan="12">Linux</td> <td rowspan="3">GCC</td><td>4.1.2</td><td>RHEL/Centos 5.3</td> <td bgcolor="#00FF00">Supported</td></tr>
+    <tr>                                                   <td>4.1.2</td><td>Linux Networx (SLES 9)</td><td bgcolor="#00FF00">Supported</td></tr>
+    <tr>                                                   <td>4.4.1</td><td>Ubuntu 9.10</td>      <td bgcolor="#00FF00">Supported</td></tr>
+    <tr>           <td rowspan="3">Pathscale</td>         <td>3.2.99</td><td>Cray XT5</td>         <td>Unknown</td></tr>
+    <tr>                                         <td rowspan="2">3.2</td><td>Cray XT4</td>         <td bgcolor="#00FF00">Supported</td></tr>
     <tr>                                                                 <td>Cray XT3</td>         <td>Unknown</td></tr>
-    <tr><td rowspan="2">Cygwin</td><td rowspan="2">GCC</td><td>3.5</td>  <td></td>                 <td>Supported</td></tr>
-    <tr>                                                   <td>4.x</td>  <td></td>                 <td>Not Supported</td></tr>
+    <tr>           <td rowspan="2">PGI</td>                <td>9.0.4</td><td>Cray XT4</td>         <td bgcolor="#FFF030">Linkage problems--appears to be related to
+                                                                                                       PGI bug already reported
+                                                                                                       (see <a href="http://www.pgroup.com/userforum/viewtopic.php?p=5367">
+                                                                                                       http://www.pgroup.com/userforum/viewtopic.php?p=5367</a>).
+                                                                                                       Actually, it appears that the problem is only related to linking
+                                                                                                       (sometimes) against Boost unit_test_framework.  So, although some
+                                                                                                       of the the test suite might not build, it might still work.  Note
+                                                                                                       that of the approximately twenty CHIMP unit tests, three fail to
+                                                                                                       link and run successfully.
+                                                                                                       </td></tr>
+    <tr>                                                   <td>8.0.5</td><td>Cray XT4</td>         <td bgcolor="#FFF030">Same Problems as PGI 9.0.4</td></tr>
+    <tr>           <td rowspan="4">Intel</td>               <td>10.1</td><td>RHEL/Centos 5.3</td>  <td bgcolor="#00FF00">Supported</td></tr>
+    <tr>                                                    <td>11.0</td><td></td>                 <td bgcolor="#FF3030">Unsupported--<a href="#intel11.0">Preprocessor bug exposed</a></td></tr>
+    <tr>                                        <td rowspan="2">11.1</td><td>Linux Networx (SLES 9)</td><td bgcolor="#00FF00">Supported</td></tr>
+    <tr>                                                                 <td>Dell PowerEdge M610 (RHEL?)</td><td bgcolor="#00FF00">Supported</td></tr>
+    <tr><td rowspan="2">Cygwin</td><td rowspan="2">GCC</td><td>3.5</td>  <td></td>                 <td bgcolor="#00FF00">Supported</td></tr>
+    <tr>                                                   <td>4.x</td>  <td></td>                 <td bgcolor="#FF3030">Unsupported</td></tr>
   </table>
+
+  <hr>
+  <b><a name="intel11.0">Intel 11.0 has a preprocessor bug</a></b> that is
+  exposed by the relative ordering of
+  <code>\#include <physical/physical.h></code> and
+  <code>\#include <physical/runtime.h></code> directives.  It seems that
+  Intel 11.0 does not re-read some files even if the header guards allow for it.
+  Placing <code>\#warning</code> directives appears to alter the functionality
+  of the preprocessor.  Although a judicious reordering of the
+  <code>\#include</code> directives causes the builds to succeed, I would rather
+  not support a compiler that shows such a blatant bug.  
 */
